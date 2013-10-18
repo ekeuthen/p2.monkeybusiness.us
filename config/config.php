d13856296362b76f078c2c7c0c88635dac59e54e
Emily Keuthen <ekeuthen@gmail.com>
2013-10-17 21:55:55 -0400

    changing db name

--- config/config.php
+++ config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', "p2_monkeybusiness_us"); 
+	define('DB_NAME', "monkeyb1_p2_monkeybusiness_us"); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
